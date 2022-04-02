cc1cd8130915a44e43a5c9a9b1e4d4f569206559
Bjorn Slingerland <61951932+bjornslingerland@users.noreply.github.com>
2022-04-02 14:26:24 +0200

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+var menuButton = document.getElementById('menuBekijkenButton')
+menuButton.addEventListener('click', () => {
+    window.open(location.origin + '/assets/Menu.pdf', '_blank').focus();
+})
+
+var menuButton2 = document.getElementById('menuBekijkenButton2')
+menuButton2.addEventListener('click', () => {
+    window.open(location.origin + '/assets/Menu.pdf', '_blank').focus();
+})
